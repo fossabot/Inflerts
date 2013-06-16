103db736a85c7c85f0da2b965977d05dd657430a
polyfish42 <polyfish42@gmail.com>
2013-06-16 15:50:34 -0700

    added first contact

--- web.rb
+++ web.rb
@@ -3,8 +3,13 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/web' do
+  sender = params[:From]
+  friends = {
+    "+17162399248" => "Jacob Brady",
+  }
+  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hey Monkey. Thanks for the message!"
+    r.Sms "Hello, #{name}. Thanks for the message."
   end
   twiml.text
 end
