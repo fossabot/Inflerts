289950c2bcf82fdf75ddb9728cb4e9ab168abc02
polyfish42 <polyfish42@gmail.com>
2013-06-16 19:09:13 -0700

    test

--- forward-sms.rb
+++ forward-sms.rb
@@ -3,20 +3,13 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/forward-sms' do	
-  sender = params[:From]
-  friends = {
-    "+7162399248" => "Jake",
-  }
-  bodytext = params[:Body]
-  from = "+17747664115"
   account_sid = "ACff661d56b0d1fdbc31917d5fd0f6c05b"
-  auth_token = "5995c907bf237c2a8570d8fa0c31ed37"
+	auth_token = "5995c907bf237c2a8570d8fa0c31ed37"
   client = Twilio::REST::Client.new account_sid, auth_token
 
   client.account.sms.messages.create(
-    :from => from,
+    :from => "+17747664115",
     :to => "+7162399248",
-    :body => "#{bodytext}"
-  )
-  puts "Sent message to #{bodytext}" 
+    :body => "hello"
+  ) 
 end
