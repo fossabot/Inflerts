c60b221e7ed54a63be0ed9a5d953125b82407151
polyfish42 <polyfish42@gmail.com>
2013-06-16 19:18:22 -0700

    whoa

--- forward-sms.rb
+++ forward-sms.rb
@@ -2,22 +2,18 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
  
-get '/forward-sms' do	
+ get '/forward-sms' do	
   account_sid = "ACff661d56b0d1fdbc31917d5fd0f6c05b"
   auth_token = "5995c907bf237c2a8570d8fa0c31ed37"
   client = Twilio::REST::Client.new account_sid, auth_token
  
   from = "+17747664115" # Your Twilio number
  
-  friends = {
-  "+17162399248" => "Jake",
-  }
-  friends.each do |key, value|
     client.account.sms.messages.create(
       :from => from,
-      :to => key,
-      :body => "Hey #{value}, Welcome to Pismo Beach!"
+      :to => "+17162399248",
+      :body => "Hey, Welcome to Pismo Beach!"
     ) 
-  puts "Sent message to #{value}"
+  puts "Sent message to"
   end
 end
