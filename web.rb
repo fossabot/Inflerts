98018047966a122d4aa13ac3897825e840393ebd
polyfish42 <polyfish42@gmail.com>
2013-06-16 17:06:01 -0700

    added some cool twilio stuff

--- web.rb
+++ web.rb
@@ -1,5 +1,20 @@
-require 'sinatra'
-
-get '/' do
-  "Hello, world"
+require 'rubygems'
+require 'twilio-ruby'
+ 
+account_sid = "ACff661d56b0d1fdbc31917d5fd0f6c05b"
+auth_token = "5995c907bf237c2a8570d8fa0c31ed37"
+client = Twilio::REST::Client.new account_sid, auth_token
+ 
+from = "+17747664115" # Your Twilio number
+ 
+friends = {
+"+17162399248" => "Jake",
+}
+friends.each do |key, value|
+  client.account.sms.messages.create(
+    :from => from,
+    :to => key,
+    :body => "Hey #{value}, Monkey party at 6PM. Bring Bananas!"
+  ) 
+  puts "Sent message to #{value}"
 end
