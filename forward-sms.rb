5f502e761ee2bb135cca7f367b04975ceca4ec15
polyfish42 <polyfish42@gmail.com>
2013-06-16 18:53:02 -0700

    this is getting exciting

--- forward-sms.rb
+++ forward-sms.rb
@@ -3,8 +3,14 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/forward-sms' do	
+  sender = params[:From]
+  friends = {
+    "+7162399248" => "Jake",
+  }
+  bodytext = params[:Body]
+  fowardmessage = friends[sender] || "empty"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hey Monkey. Thanks for the message!"
+    r.Sms "#{fowardmessage}"
   end
   twiml.text
 end
