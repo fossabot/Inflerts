f336b2b65b81f2c75aa53b7fa142e1e200aa8eb6
polyfish42 <polyfish42@gmail.com>
2013-06-16 18:46:10 -0700

    added iron stuff

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 gem 'sinatra', '1.1.0'
 gem 'twilio-ruby'
+egm 'iron_worker_ng'
--- forward-sms.rb
+++ forward-sms.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
  
-get '/forward-sms' do
+get '/forward-sms' do	
   twiml = Twilio::TwiML::Response.new do |r|
     r.Sms "Hey Monkey. Thanks for the message!"
   end
