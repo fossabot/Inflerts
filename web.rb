b55303cc3bb0b5e67e2ec28d4cf668e4767f5f5c
polyfish42 <polyfish42@gmail.com>
2013-06-16 17:25:28 -0700

    now we're getting somewhere

--- web.rb
+++ web.rb
@@ -3,13 +3,8 @@ require 'twilio-ruby'
 require 'sinatra'
  
 get '/web' do
-  sender = params[:From]
-  friends = {
-    "+17162399248" => "Jake",
-  }
-  name = friends[sender] || "Mobile Monkey"
   twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hello, #{name}. Thanks for the message."
+    r.Sms "Hey Monkey. Thanks for the message!"
   end
   twiml.text
 end
