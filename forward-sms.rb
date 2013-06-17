6536b048a5d3caeb6089ca14a33f524463885f1b
polyfish42 <polyfish42@gmail.com>
2013-06-16 19:15:26 -0700

    test

--- forward-sms.rb
+++ forward-sms.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'twilio-ruby'
 require 'sinatra'
  
+get '/forward-sms' do	
   account_sid = "ACff661d56b0d1fdbc31917d5fd0f6c05b"
   auth_token = "5995c907bf237c2a8570d8fa0c31ed37"
   client = Twilio::REST::Client.new account_sid, auth_token
@@ -18,4 +19,5 @@ require 'sinatra'
       :body => "Hey #{value}, Welcome to Pismo Beach!"
     ) 
   puts "Sent message to #{value}"
+  end
 end
