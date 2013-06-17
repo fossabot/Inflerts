c6a4bcfb8573c76a6c708adf69aee571ac8ed421
polyfish42 <polyfish42@gmail.com>
2013-06-16 17:01:54 -0700

    init

--- web.rb
+++ web.rb
@@ -1,15 +1,5 @@
-require 'rubygems'
-require 'twilio-ruby'
 require 'sinatra'
- 
-get '/web' do
-  sender = params[:From]
-  friends = {
-    "+17162399248" => "Jacob Brady",
-  }
-  name = friends[sender] || "Mobile Monkey"
-  twiml = Twilio::TwiML::Response.new do |r|
-    r.Sms "Hello, #{name}. Thanks for the message."
-  end
-  twiml.text
+
+get '/' do
+  "Hello, world"
 end
