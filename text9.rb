0630ad563656bc31f0118c5b735abca67bd26fb6
polyfish42 <polyfish42@gmail.com>
2013-06-20 10:05:17 -0700

    test

---
+++ text9.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'twilio-ruby'
+ 
+ACCOUNT_SID = "ACff661d56b0d1fdbc31917d5fd0f6c05b"
+AUTH_TOKEN = "5995c907bf237c2a8570d8fa0c31ed37"
+
+client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+ 
+FROM = "+17747664115" # Your Twilio number
+ 
+ADMINS = {
+    "+17162399248" => "Jake",
+    "+14085823425" => "Hamida",
+    "+16504215192" => "Terri",
+  }
+COMPANY = {
+
+}.merge(ADMINS)
+
+COMPANY.each do |phone, firstname|
+  client.account.sms.messages.create(
+    :from => FROM,
+    :to => phone,
+    :body => "Good Morning. Shuttle leaving for Inflection from hotel lobby in+1 hour. We'll have advil on the bus."
+  ) 
+end
